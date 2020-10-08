3f9a83e61152c4d77eb47d8c530a9b29e1336eef
Nadanai Kurairat <614259015@webmail.npru.ac.th>
2020-10-08 21:13:46 +0700

    commit1

--- index.js
+++ index.js
@@ -7,20 +7,26 @@ const restaurantRouter = require('./routes/restaurant');
 const indexRouter = require('./routes/index');
 //creater Server
 const app = express();
-/*app.get('/',(req,res) => {
-    res.send('<h1>Hello Restaurant API <h1>');
-});*/
-//Template engine setting
+
+//template engine setting
 app.engine('hbs',hbs({extname:'hbs'}));
 app.set('view engine', 'hbs');
+
 //Middleware เพื่ออ่าน req.body
-app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 
-
-//router
+//Use Router
 app.use('/api',restaurantRouter);
-app.listen(3000,()=>{
-    console.log('Listening to port 3000');
-});
+app.use('/',indexRouter);
+
+//Middleware
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(
+    PORT,
+    () => {
+      console.log(`Listening to port ${PORT}`);
+    }
+  );
+  
